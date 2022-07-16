b8867fcf239cf780d35ecba7b0b131ae3b0c886e
Firu1996 <chayakorn.f.ch@gmail.com>
2022-07-16 17:37:48 +0700

    add Signout

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { useSelector } from 'react-redux'
-
+import { useSelector, useDispatch } from 'react-redux'
+import { setAuth } from '../actions/authAction'
 export default function Nav() {
   const cart = useSelector((state) => state.cart)
   const { user } = useSelector((state) => state.auth)
-
+  const dispatch = useDispatch();
   return (
     <header className='head'>
       <div>
@@ -20,7 +20,7 @@ export default function Nav() {
               </span></Link>
           </li>}
           <li className='nav-list'>
-            {user ? <button>Sign out</button> : <Link to='/signin'>Sign in</Link>}
+            {user ? <button onClick={() => dispatch(setAuth(null))}>Sign out</button> : <Link to='/signin'>Sign in</Link>}
           </li>
         </ul>
       </div>
