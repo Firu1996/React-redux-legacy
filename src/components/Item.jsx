b3f9fc9e2033629ed7b86e8cdf11431fecd00cb1
Firu1996 <chayakorn.f.ch@gmail.com>
2022-07-16 17:37:34 +0700

    add logic for login

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,14 +1,25 @@
 import React from 'react'
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { addTocart } from '../actions/cartActions'
-
+import { useHistory } from 'react-router-dom';
 export default function Item({ product }) {
   const dispatch = useDispatch()
+  const { user } = useSelector(state => state.auth)
+  const history = useHistory()
+
+  const handleAddToCart = () => {
+    if (!user) {
+      history.push('/signin')
+    } else {
+      dispatch(addTocart({ ...product, qty: 1 }))
+    }
+  }
+
   return (
     <div className='product' >
       <h4>{product.title}</h4>
       <p>{product.price}</p>
-      <button onClick={() => dispatch(addTocart({ ...product, qty: 1 }))}>
+      <button onClick={() => handleAddToCart()}>
         Add To Cart
       </button>
     </div >
