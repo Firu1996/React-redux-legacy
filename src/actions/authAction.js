d9a1b0458e46cca5f4868f9a2a5854de6bff3ee9
Firu1996 <chayakorn.f.ch@gmail.com>
2022-07-16 17:37:21 +0700

    add fetchUser

--- src/actions/authAction.js
+++ src/actions/authAction.js
@@ -1,3 +1,6 @@
+import { startFetch, endFetch, errorFetch } from '../actions/statusAction'
+import { signin } from '../data/users'
+
 export const SET_AUTH = 'SET_AUTH'
 
 
@@ -5,5 +8,23 @@ export function setAuth(user) {
     return {
         type: SET_AUTH,
         payload: user
+    }
+}
+
+export function fetchAuthAsync(email, password) {
+    return async function (dispatch, getState) {
+        try {
+            dispatch(startFetch());
+            const user = await signin(email, password);
+            if (user) {
+                dispatch(setAuth(user))
+                dispatch(errorFetch(''))
+                dispatch(endFetch())
+            }
+        } catch (err) {
+            dispatch(setAuth(null))
+            dispatch(errorFetch(err))
+            dispatch(endFetch())
+        }
     }
 }
