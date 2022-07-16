090d9d6c0ee4da0ddf57d2065811d827b39089ad
Firu1996 <chayakorn.f.ch@gmail.com>
2022-07-16 16:25:40 +0700

    Add logic for add duplicate item

--- src/reducers/cartReducer.js
+++ src/reducers/cartReducer.js
@@ -17,10 +17,22 @@ const initialState = {
 export function cartReducer(state = initialState, action) {
     switch (action.type) {
         case ADD_TO_CART:
-            const updatedState = [...state.cart, action.payload]
+            let updatedCart;
+
+            const foundItem = state.cart.find(item => item.id === action.payload.id)
+
+            if (!foundItem) {
+                updatedCart = [...state.cart, action.payload]
+            } else {
+                updatedCart = state.cart.map(item => ({
+                    ...item,
+                    qty: item.id === foundItem.id ? item.qty + 1 : item.qty
+                }))
+            }
+
             return {
                 ...state,
-                cart: updatedState
+                cart: updatedCart
             }
             break;
         default:
